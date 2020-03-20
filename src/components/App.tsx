3afe511809ca8d8240828491d818263ca3dbcc51
Raymond Johannessen <rajohan1@gmail.com>
2020-03-20 20:49:57 +0100

    first commit

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { Switch, Route } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 
-function App() {
-    return <div className="App">Test</div>;
-}
+import { testAction, StoreContext } from "../store/Store";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+    }
+    
+     body {
+        background-color: #262626;
+    }
+    
+    #root {
+        color: #fff;
+        max-width: 100vw;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-bottom: 40px;
+    }
+`;
+
+const App: React.FC = () => {
+    const { state, dispatch } = useContext(StoreContext);
+
+    useEffect(() => {
+        dispatch(testAction());
+    }, [dispatch]);
+
+    return (
+        <React.Fragment>
+            <GlobalStyle />
+            <Switch>
+                <Route path="/" exact>
+                    Home {console.log(state.test)}
+                </Route>
+                <Route path="/contact">Contact</Route>
+                <Route path="/favourites">Favourites</Route>
+                <Route path="/game/:id">Game</Route>
+            </Switch>
+        </React.Fragment>
+    );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { StoreProvider } from "./store/Store";
+import App from "./components/App";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Root: React.FC = () => {
+    return (
+        <StoreProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </StoreProvider>
+    );
+};
+
+ReactDOM.render(<Root />, document.getElementById("root"));
